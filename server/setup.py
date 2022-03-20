d99cb74497a9161569ffcb33e11c40a225c981be
Jacob Zhang <jacob-z@live.de>
2022-03-20 21:50:41 +0100

    cx_freeze build

---
+++ server/setup.py
@@ -0,0 +1,19 @@
+from cx_Freeze import setup, Executable
+
+# Dependencies are automatically detected, but it might need
+# fine tuning.
+build_options = {'packages': ["tensorflow_datasets", "tf_slim.layers", "scipy.integrate", "scipy.optimize", "keras"],
+                 "include_files": ["assets/"],
+                 'excludes': []}
+
+base = 'Console'
+
+executables = [
+    Executable('server.py', base=base)
+]
+
+setup(name='nornsvae_server',
+      version='1.0',
+      description='',
+      options={'build_exe': build_options},
+      executables=executables)
