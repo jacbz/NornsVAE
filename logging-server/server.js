6c606da0f2b3be07e915115e566530fb5d8a66d1
Jacob Zhang <jacob-z@live.de>
2022-04-25 18:14:57 +0200

    Refactor server

--- logging-server/server.js
+++ logging-server/server.js
@@ -1,29 +1,34 @@
-const express = require('express');
+const express = require("express");
+const secrets = require("./secrets.json");
+
 const app = express();
 app.use(express.json());
-const MongoClient = require('mongodb').MongoClient
-const connectionString = 'mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false';
+const MongoClient = require("mongodb").MongoClient;
+const connectionString = `mongodb://${secrets.mongodb_user}:${secrets.mongodb_pw}@localhost:27017`;
 
-MongoClient.connect(connectionString, { useUnifiedTopology: true })
-  .then(client => {
-    console.log('Connected to Database')
-    const db = client.db('log')
-    const logCollection = db.collection('logging')
-    
-    app.listen(3000, function() {
-      console.log('listening on 3000')
-    })
+MongoClient.connect(connectionString, { useUnifiedTopology: true }).then(
+  (client) => {
+    console.log("Connected to Database");
+    const db = client.db("log");
+    const logCollection = db.collection("logging");
 
-    app.get('/', function(req, res) {
-      res.send('Logging server is running!')
-    })
+    app.listen(50000, () => {
+      console.log("listening on 50000");
+    });
 
-    app.post('/log', (req, res) => {
-      logCollection.insertMany(req.body)
-        .then(result => {
-          console.log(result)
-          res.send(req.body)
-        })
-        .catch(error => console.error(error))
-    })
-  })
+    app
+      .route("/")
+      .get((req, res) => {
+        res.send("Logging server is running!");
+      })
+      .post((req, res) => {
+        logCollection
+          .insertMany(req.body)
+          .then((result) => {
+            console.log(result);
+            res.send(req.body);
+          })
+          .catch((error) => console.error(error));
+      });
+  }
+);
